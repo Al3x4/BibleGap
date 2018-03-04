4739c1e9cd6e4bb743094b113cf1da2c90d1d5dd
Alexandra Ralev <alexandrapredaralev@gmail.com>
2018-03-04 09:36:42 +0000

    Launch first working prototype with messages in the console

--- js/app.js
+++ js/app.js
@@ -1,1 +1,52 @@
-console.log("connected");
+window.onload = function() {
+
+let correctAnswer, answerString, answerArray = [], score = 0;
+
+
+function displayQuestion(){
+	//clean up the previous answerS
+	answerArray = [];
+	answerString = "";
+
+	//get a random index
+	index = Math.floor(Math.random() * questions.length) + 1;
+	
+	//get the question text of the index
+	document.querySelector("#question").innerHTML = questions[index].text;
+	
+	//also store the correct answer for the verse displayed
+	correctAnswer = questions[index].answer;
+
+}
+
+
+
+function checkAnswer(){
+	document.querySelectorAll('.answer').forEach(function(val, index, obj){
+		answerArray.push(val.value);
+		
+	});
+
+	answerString = answerArray.join(", ");
+	console.log(answerString);
+
+	if (answerString == correctAnswer){
+		score++;
+		console.log(score);
+		displayQuestion();
+	} else {
+		console.log("the correct answer was ", correctAnswer);
+	}
+	
+}
+
+
+
+displayQuestion();
+
+document.querySelector("#submitAnswer").addEventListener("click", checkAnswer);
+
+}
+
+
+//TODO - make sure the index doesnt repeat bu sto
